dd143882362234e259fed732a768a9d70a36b8b2
Arash Ashrafzadeh <arashmath@aut.ac.ir>
2018-07-04 20:17:41 +0430

    Corrections to choose_next_state

--- AIproject - SA.py
+++ AIproject - SA.py
@@ -1,4 +1,4 @@
-#---------------- Importing needed modules ----------------
+#---------------- Importing necessary modules ----------------
 import networkx as nx # To use graph features
 import numpy as np # To input the adjacency matrix as a 2-D array
 import matplotlib.pyplot as plt # To display a graphical view of the graph
@@ -32,7 +32,7 @@ g4_adj = [[0,1,1,1],
           [1,1,0,1],
           [1,1,1,0]]
 
-adjacency_matrix = np.array(g4_adj)
+adjacency_matrix = np.array(g1_adj)
 G = nx.from_numpy_array(adjacency_matrix) # Creating the graph given the adjacency matrix
 
 #---------------- function maximum_degree ----------------
@@ -55,7 +55,7 @@ def generate_random_color(number_of_colors):
     return list_of_colors
 
 #---------------- function assign_color ----------------
-# This function receives a graph and a list of colors as input, and assigns the colors in the list to the
+# This function receives a graph and a list of colors as input, and assigns a valid color in the list to the
 # nodes of the graph and returns the result graph.
 def assign_color(graph_, colors):
 
@@ -71,23 +71,16 @@ def assign_color(graph_, colors):
 
 #---------------- function initial_state_generator ----------------
 # This function receives a graph as input, and returns a graph in a random-color state, so that
-# we can run hill-climbing algorithm on that graph. It also returns the list of colors to be used in HC.
+# we can run Simulated Annealing algorithm on that graph. It also returns the list of colors to be used in HC.
 def initial_state_generator(graph):
     max_deg = maximum_degree(graph)
     color_list = generate_random_color(max_deg+1)
     graph = assign_color(graph, color_list)
     return graph, color_list
 
-#---------------- function count_coloring ----------------
-def count_coloring(graph_):
-    color_of_nodes = []
-    for node in graph_.nodes:
-        if graph_.nodes[node]['color'] not in color_of_nodes:
-            color_of_nodes.append(graph_.nodes[node]['color'])
-    return len(color_of_nodes)
-
 #---------------- function loss_function ----------------
 # This function calculates the loss-function for each state
+# Loss value is defined to be the number of colors used for coloring a graph
 def loss_function(graph_):
     used_colors = []
     for node in graph_.nodes():
@@ -95,17 +88,14 @@ def loss_function(graph_):
             used_colors.append(graph_.nodes[node]['color'])
     return len(used_colors)
 
-#---------------- function print_graph ----------------
-def print_graph(graph_):
-    for node in graph_.nodes:
-        print(str(node) +" , "+ graph_.nodes[node]['color'])
-    print("\n")
-
 #---------------- function assign_probability ----------------
+# This function receives a graph, a node of that graph, a color and current temperature
+# and returns the probability of accepting the next state.
+# Next state is the input graph with colored input node with input color
 def assign_probability(graph_, node_, color_, t):
-    current_value = (-1) * count_coloring(graph_)
+    current_value = (-1) * loss_function(graph_)
     graph_.nodes[node_]['color'] = color_
-    next_value = (-1) * count_coloring(graph_)
+    next_value = (-1) * loss_function(graph_)
     changed_energy_level = next_value - current_value
     if (changed_energy_level >= 0):
         return 1
@@ -113,51 +103,41 @@ def assign_probability(graph_, node_, color_, t):
         return e**(changed_energy_level/t)
 
 #---------------- function choose_next_state ----------------
-def choose_next_state(nodes_dict):
-    acceptance_dict = {}
+# This function receives a dictionary of nodes (zzz) and chooses one node randomly among them all.
+def choose_next_state(graph_, nodes_dict):
     (node, color) = random.choice(list(nodes_dict.keys()))
     if nodes_dict[(node, color)] == 1:
-        return (node, color)
+        graph_.nodes[node]['color'] = color
+        return graph_
         ### change graph to new state
     else:
         ### generate random number an choose an interval, therefore next state
-        acceptance_dict[(node, color)] = nodes_dict[(node, color)]
-        sum_of_probabilities = sum(acceptance_dict.values())
-        for val in acceptance_dict.values():
-            val /= sum_of_probabilities
-        temp_sum = 0
-        for nd_cl in acceptance_dict.keys():
-            acceptance_dict[nd_cl] = (temp_sum, acceptance_dict[nd_cl]+temp_sum) ### might have an error
-            temp_sum = copy.deepcopy(acceptance_dict[nd_cl])
+        rand = random.random()
+        if (rand<=nodes_dict[(node, color)]):
+            graph_.nodes[node]['color'] = color
+            return graph_
+        else:
+            return graph_
 
-        while(True):
-            for key, val in acceptance_dict.items():
-                rand = random.random()
-                if ((rand >= val[0]) and (rand <= val[1])):
-                    return key
-
-#---------------- function Simulated_annealing ----------------
+#---------------- function simulated_annealing ----------------
 def simulated_annealing(graph, colors, t):
     if t == 0:
         return graph
 
-    # print_graph(graph)
     candidate_colored_nodes = {}
 
     for node in graph.nodes():
         node_initial_color = copy.deepcopy(graph.nodes[node]['color'])
         adjacent_colors = []
         for adj_node in graph.adj[node]:
-            if graph.nodes[adj_node]['color'] not in adjacent_colors:
-                adjacent_colors.append(graph.nodes[adj_node]['color'])
+            adjacent_colors.append(graph.nodes[adj_node]['color'])
 
         for color in colors: # check all the colors and compare with each other
-            if color not in adjacent_colors:
+            if color not in set(adjacent_colors):
                 candidate_colored_nodes[(node, color)] = assign_probability(graph, node, color, t)
 
         graph.nodes[node]['color'] = node_initial_color
-    nd, cl = choose_next_state(candidate_colored_nodes)
-    graph.nodes[nd]['color'] = cl
+    graph = choose_next_state(graph, candidate_colored_nodes)
     t -= 1
     graph = simulated_annealing(graph, color_list, t)
     return graph
